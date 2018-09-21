c3d90c85bcf368f45b2129f685ad3931b241cfe0
Lodis Perkins <s188043@students.aie.edu>
2018-09-21 09:29:04 -0500

    teacher helps by deleting everything.....

--- Lab00/Game.cpp
+++ Lab00/Game.cpp
@@ -1,6 +1,9 @@
 #include "Game.h"
 
-Hero Game::Battle(Hero & hero, Hero & hero2)
-{
-	
+Hero Game::Battle(Hero & hero1, Hero & hero2)
+{	
+	//we fight hero1 against hero 2	
+	hero1.Fight(hero2);
+	hero2.Fight(hero1);
+	//we then fight hero2 against hero1 but ONLY if they are both alive
 }
