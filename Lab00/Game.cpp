f9f27458032edc9e6f4a51b4525931f402967037
Lodis Perkins <s188043@students.aie.edu>
2018-09-21 09:22:28 -0500

    legit before mr matt

--- Lab00/Game.cpp
+++ Lab00/Game.cpp
@@ -2,5 +2,39 @@
 
 Hero Game::Battle(Hero & hero, Hero & hero2)
 {
+	bool life = hero.isAlive();
+	bool life2 = hero2.isAlive();
+	while (life == true || life2 == true)
+	{
+		bool life = hero.isAlive();
+		
+		if(life == false)
+		{
+			break;
+		}
+		else 
+		{
+			hero.Fight(hero2);
+		}
+		bool life2 = hero2.isAlive();
+		if (life2 == false)
+		{
+			break;
+		}
+		else
+		{
+			hero2.Fight(hero);
+		}
+	}
 	
+	if (life == true)
+	{
+		std::cout << "Iron Man wins!!\n";
+		return hero;
+	}
+	else if (life2== true)
+	{
+		std::cout << "Captain America wins!!\n";
+		return hero2;
+	}
 }
